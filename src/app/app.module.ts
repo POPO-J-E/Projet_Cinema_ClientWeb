a6e660d3ecbf25d85492c753e1488dcc13841563
DOLLE <jeremy.dolle34@gmail.com>
2017-10-13 13:28:22 +0200

    use MDBoostrap Angular component :art:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -19,11 +18,10 @@ import { SlideComponent } from './slide/slide.component';
     SlideComponent
   ],
   imports: [
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
+    MDBBootstrapModule.forRoot(),
     BrowserModule
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
